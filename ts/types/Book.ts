a2899583744534f604085711ae5c3b6a3e77e8dc
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2026-02-03 00:18:35 +0100

    Add types for the books and json for books

---
+++ ts/types/Book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+    id: number;
+    title: string;
+    author: string;
+    genre: string[];
+    price: number;
+    stock: number;
+    image: string;
+    description: string;
+    isbn: string;
+}
